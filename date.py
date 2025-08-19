d854e36b1b66302d069298ed98e7baff12c1fb7d
Bhagyakj <bhagyalakshmikj02@gmail.com>
2025-08-19 15:02:21 +0530

    Add files via upload

---
+++ date.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Generate the date range
+date_series = pd.date_range(start='2024-11-20', end='2024-11-24')
+
+# Print the result
+print("Date series from 20th November 2024 to 24th November 2024:")
+print(date_series)
